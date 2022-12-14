5b6c560038db8034d430a12b273feb814e3febfa
afroiland <andrew.froiland@gmail.com>
2022-12-14 08:35:29 -0600

    initial commit

---
+++ Blazor Server Testing/Data/DBConnection.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Blazor_Server_Testing.Models;
+using System.Collections.Generic;
+
+namespace Blazor_Server_Testing.DB_Connection
+
+{
+    public class DBConnection
+    {
+        public static List<CharacterModel> QueryDB(string connectionString, string query)
+        {
+            List<CharacterModel> listResult = new();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var result = connection.Query<CharacterModel>(query);
+
+                foreach (var x in result)
+                {
+                    listResult.Add(x);
+                }
+            }
+
+            return listResult;
+        }
+    }
+}
